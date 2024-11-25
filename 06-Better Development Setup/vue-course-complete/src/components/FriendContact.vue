183d5298c1ebc610f216b568d084e3943eaadf7d
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2024-11-25 15:28:56 +0430

    FreindContact component created

---
+++ 06-Better Development Setup/vue-course-complete/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{detailsAreVisible?'Hide':'Show'}}Details</button>
+        <ul v-if="detailsAreVisible">
+            <li>{{ friend.phone }}</li>
+            <li>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        detailsAreVisible: false,
+      friend: {
+        id: "jawad",
+        name: "Jawad Mujaddidi",
+        phone: "0784801901",
+        email: "jawad@gmail.com",
+      },
+    };
+  },
+  methods:{
+    toggleDetails(){
+        this.detailsAreVisible=!this.detailsAreVisible
+    }
+  }
+};
+</script>
+
+
