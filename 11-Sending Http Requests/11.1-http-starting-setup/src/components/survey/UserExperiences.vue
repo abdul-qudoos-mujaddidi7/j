a944b10daa0a4f194efe8b0199bd5b3d77a9ce9e
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2024-12-05 16:14:20 +0430

    Http Get Request & Loading added

--- 11-Sending Http Requests/11.1-http-starting-setup/src/components/survey/UserExperiences.vue
+++ 11-Sending Http Requests/11.1-http-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,9 +3,10 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if='isLoading'>Loading.....</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -18,13 +19,52 @@
 </template>
 
 <script>
+
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  data(){
+    return{
+      results:[],
+      isLoading:false
+    }
+  },
+  // props: ['results'],
   components: {
     SurveyResult,
   },
+
+methods: {
+  loadExperiences() {
+    this.isLoading=true
+    fetch('https://vue-http-demo-7-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Failed to fetch'); // Optional: Handle errors
+      })
+      .then(data => {
+        this.isLoading=false
+        const results = [];
+        for (const id in data) {
+          results.push({
+            id: id,
+            name: data[id].name,    
+            rating: data[id].rating
+          });
+        }
+        this.results = results; 
+      })
+  }
+},
+
+mounted(){
+  this.loadExperiences()
+}
+
+  // response.json() is a method used to extract JSON data from an HTTP response.
+  //  It reads the response body and converts it into a JavaScript object or array.
 };
 </script>
 
