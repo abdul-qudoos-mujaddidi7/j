6697b28e365300bb2f9f22e5800243a4e5fb2c2b
abdul-qudoos-mujaddidi7 <abdulqudoosmujaddidi7@gmail.com>
2024-12-23 11:23:16 +0430

    router view&link

--- 12-Routing/routing-01-starting-setup/src/components/nav/TheNavigation.vue
+++ 12-Routing/routing-01-starting-setup/src/components/nav/TheNavigation.vue
@@ -3,10 +3,10 @@
     <nav>
       <ul>
         <li>
-          <button @click="setActivePage('teams-list')">Teams</button>
+          <router-link to='/teams'>Teams</router-link>
         </li>
         <li>
-          <button @click="setActivePage('users-list')">Users</button>
+          <router-link to='/users'>Users</router-link>
         </li>
       </ul>
     </nav>
@@ -49,7 +49,7 @@ li {
   margin: 0 2rem;
 }
 
-button {
+a{
   font: inherit;
   background: transparent;
   border: 1px solid transparent;
@@ -59,8 +59,9 @@ button {
   display: inline-block;
 }
 
-button:hover,
-button:active {
+a:hover,
+a:active,
+a.router-link-active {
   color: #f1a80a;
   border-color: #f1a80a;
   background-color: #1a037e;
--- 12-Routing/routing-01-starting-setup/src/main.js
+++ 12-Routing/routing-01-starting-setup/src/main.js
@@ -1,17 +1,17 @@
 import { createApp } from 'vue';
 import {createRouter,createWebHashHistory} from 'vue-router'
-import TeamList from './components/teams/TeamList.vue'
-import UserList from './components/users/UserList.vue'
+import TeamsList from './components/teams/TeamsList.vue'
+import UsersList from './components/users/UsersList.vue'
 import App from './App.vue';
 
 const router=createRouter({
     history:createWebHashHistory(),
     routes:[
-        {path:'/teams' , component:TeamList}, // our.domain.com.teams => TeamList
-        {path:'/users', component:  UserList}
+        {path:'/teams' , component:TeamsList}, // our.domain.com.teams => TeamList
+        {path:'/users', component:  UsersList}
 
     ],
 });
 const app = createApp(App);
-app.use(router)
+app.use(router);
 app.mount('#app');
